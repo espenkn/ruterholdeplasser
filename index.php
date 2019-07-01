a9dd71d4d914e21824581e8c16e5e3ca5d62ab29
Espen Nilsen <espen.k.nilsen@gmail.com>
2019-07-01 14:16:37 +0200

    ny tavle løsning

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <?php
 
-var_dump($_SERVER['HTTP_HOST']);
-
-var_dump($_SERVER['SERVER_NAME']);
-die();
-
-if ($_SERVER['HTTP_HOST'])
-{
-
-}
-
-
-$default = 'esn';
-
-$config = [
-    'leo' => 'https://tavla.en-tur.no/dashboard/@59-94538338903199,10-791405986479825/eyJkaXN0YW5jZSI6IjUyNiIsImhpZGRlblN0YXRpb25zIjpbXSwiaGlkZGVuU3RvcHMiOlsiTlNSOlN0b3BQbGFjZTo2MTQxIiwiTlNSOlN0b3BQbGFjZTo1OTY0MyIsIk5TUjpTdG9wUGxhY2U6NTgzMDYiLCJOU1I6U3RvcFBsYWNlOjU4MzA1IiwiTlNSOlN0b3BQbGFjZTo2MDI4Il0sImhpZGRlblJvdXRlcyI6W10sImhpZGRlbk1vZGVzIjpbXSwibmV3U3RhdGlvbnMiOltdLCJuZXdTdG9wcyI6WyJOU1I6U3RvcFBsYWNlOjU5NjQzIiwiTlNSOlN0b3BQbGFjZTo1ODMwOSIsIk5TUjpTdG9wUGxhY2U6NTk4NzgiLCJOU1I6U3RvcFBsYWNlOjU5NjQ4Il19',
-    'esn' => 'https://tavla.en-tur.no/dashboard/@59-926348,10-776113/eyJkaXN0YW5jZSI6IjU1OCIsImhpZGRlblN0YXRpb25zIjpbXSwiaGlkZGVuU3RvcHMiOlsiTlNSOlN0b3BQbGFjZTo2NDUxIiwiTlNSOlN0b3BQbGFjZTo1ODE2MiIsIk5TUjpTdG9wUGxhY2U6NTgyNTUiLCJOU1I6U3RvcFBsYWNlOjY0NTMiXSwiaGlkZGVuUm91dGVzIjpbXSwiaGlkZGVuTW9kZXMiOltdLCJuZXdTdGF0aW9ucyI6W10sIm5ld1N0b3BzIjpbXX0=',
+$boards = [
+    'ruter.kleinelectronics.no' => 'https://tavla.en-tur.no/dashboard/@59-926348,10-776113/eyJkaXN0YW5jZSI6IjU1OCIsImhpZGRlblN0YXRpb25zIjpbXSwiaGlkZGVuU3RvcHMiOlsiTlNSOlN0b3BQbGFjZTo2NDUxIiwiTlNSOlN0b3BQbGFjZTo1ODE2MiIsIk5TUjpTdG9wUGxhY2U6NTgyNTUiLCJOU1I6U3RvcFBsYWNlOjY0NTMiXSwiaGlkZGVuUm91dGVzIjpbXSwiaGlkZGVuTW9kZXMiOltdLCJuZXdTdGF0aW9ucyI6W10sIm5ld1N0b3BzIjpbXX0=',
+    'leoruter.kleinelectronics.no' => 'https://tavla.en-tur.no/dashboard/@59-94538338903199,10-791405986479825/eyJkaXN0YW5jZSI6IjUyNiIsImhpZGRlblN0YXRpb25zIjpbXSwiaGlkZGVuU3RvcHMiOlsiTlNSOlN0b3BQbGFjZTo2MTQxIiwiTlNSOlN0b3BQbGFjZTo1OTY0MyIsIk5TUjpTdG9wUGxhY2U6NTgzMDYiLCJOU1I6U3RvcFBsYWNlOjU4MzA1IiwiTlNSOlN0b3BQbGFjZTo2MDI4Il0sImhpZGRlblJvdXRlcyI6W10sImhpZGRlbk1vZGVzIjpbXSwibmV3U3RhdGlvbnMiOltdLCJuZXdTdG9wcyI6WyJOU1I6U3RvcFBsYWNlOjU5NjQzIiwiTlNSOlN0b3BQbGFjZTo1ODMwOSIsIk5TUjpTdG9wUGxhY2U6NTk4NzgiLCJOU1I6U3RvcFBsYWNlOjU5NjQ4Il19',
 ];
 
-header('Location: ' . '');
+if (!array_key_exists($_SERVER['SERVER_NAME'], $boards))
+{
+
+    if (version_compare(PHP_VERSION, '7.3.0') >= 0) 
+    {
+        $_SERVER['SERVER_NAME'] = array_key_first($boards);
+    }
+    else 
+    {
+        $_SERVER['SERVER_NAME'] = array_keys($boards)[0];
+    }
+}
+
+header('Location: ' . $boards[$_SERVER['SERVER_NAME']]);
 
